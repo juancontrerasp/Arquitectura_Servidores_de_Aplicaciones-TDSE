291798c72f05018aefcd013879f4421cd9b3151b
juancontrerasp <juan.contreras-p@mail.escuelaing.edu.co>
2026-03-05 19:45:27 -0500

    Feat: Request param annotation implementation

--- src/main/java/org/example/demo/DemoApplication.java
+++ src/main/java/org/example/demo/DemoApplication.java
@@ -17,7 +17,21 @@ public class DemoApplication {
 					System.out.println("Registering route: " + path + " -> " + m.getName());
 					HttpServer.get(path, (req, res) -> {
 						try {
-							return (String) m.invoke(null);
+							java.lang.reflect.Parameter[] params = m.getParameters();
+							if (params.length == 0) {
+								return (String) m.invoke(null);
+							}
+							Object[] args2 = new Object[params.length];
+							for (int i = 0; i < params.length; i++) {
+								RequestParam rp = params[i].getAnnotation(RequestParam.class);
+								if (rp != null) {
+									String val = req.getValues(rp.value());
+									args2[i] = (val != null) ? val : rp.defaultValue();
+								} else {
+									args2[i] = null;
+								}
+							}
+							return (String) m.invoke(null, args2);
 						} catch (Exception e) {
 							return "Error: " + e.getMessage();
 						}
